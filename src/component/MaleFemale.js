d1ccec44a76ee452e89cf8b790a602eb090a1200
Muhammad Anas <Anasnaseem420@gmail.com>
2023-01-26 02:07:38 +0500

    make male female component

---
+++ src/component/MaleFemale.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const MaleFemale = () => {
+  return (
+    <div className='un-main'>
+        <h1>Test</h1>
+    </div>
+  )
+}
+
+export default MaleFemale
--- src/component/UserName.js
+++ src/component/UserName.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import './userName.css'
+import React from 'react';
+import './userName.css';
+import MaleFemale from './MaleFemale';
+import { Link } from 'react-router-dom';
 
-const UserName = () => {
-  return (
-    <div className='un-main'>
-        <div className='un-box'>
-        <h1>what is your <br /><span className='sp-1'>Name </span></h1>
-        <img src='https://cdn-icons-png.flaticon.com/512/3094/3094866.png' alt='logo' className='un-img' />
-        </div>
-    </div>
-  )
-}
+
+class UserName extends React.Component {
+  state = {
+    testcomponent: false,
+  }
+  render() {
+    return (
+      <div>
+        {this.state.testcomponent ? <MaleFemale /> : <div className='un-main'>
+          <div className='un-box'>
+            <h1>what is your <br /><span className='sp-1'>Name </span></h1>
+            <img src='https://cdn-icons-png.flaticon.com/512/3094/3094866.png' alt='logo' className='un-img' />
+            <Link to='/' className='btn'>create quiz</Link>
+          </div>
+        </div>}
+      </div>
+    )
+  }
+};
 
 export default UserName
