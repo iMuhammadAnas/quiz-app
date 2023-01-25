455e364c2c53c47f6517199c92478c26dbefcce1
Muhammad Ejaz <mejaz4349@gmail.com>
2023-01-26 03:40:12 +0500

    set up input with validation

--- src/component/UserName.js
+++ src/component/UserName.js
@@ -4,17 +4,41 @@ import MaleFemale from './MaleFemale';
 import { Link } from 'react-router-dom';
 
 
+
 class UserName extends React.Component {
   state = {
     testcomponent: false,
+    input:'',
+    error:''
   }
   render() {
+    const onChange = (e)=>{
+      const letters = /^[A-Za-z]+$/;
+      if(!e.target.value.match(letters) && this.state.input.length){
+
+        this.setState({error:'Symbols are not allowed'})
+        console.log(e.target.value)
+      }else{
+
+        this.setState({input:e.target.value,error:''})
+      }
+    }
+    const onchangeInput = ()=>{
+      if(!this.state.input){
+        this.setState({error:'Please write Your user Name'})
+      }
+    }
     return (
       <div>
         {this.state.testcomponent ? <MaleFemale /> : <div className='un-main'>
           <div className='un-box'>
             <h1>what is your <br /><span className='sp-1'>Name </span></h1>
             <img src='https://cdn-icons-png.flaticon.com/512/3094/3094866.png' alt='logo' className='un-img' />
+            <div className='inputsec'>
+              <span>{this.state.error}</span> <br/>
+              <input onChange={onChange} onBlur={onchangeInput} type='text' width={20} />
+
+            </div>
             <Link to='/' className='btn'>create quiz</Link>
           </div>
         </div>}
