3e4c7b1017373d2df6630b326fc90790ac030ac5
Muhammad Anas <Anasnaseem420@gmail.com>
2023-01-27 00:07:15 +0500

    styling user-name input

--- src/component/UserName.js
+++ src/component/UserName.js
@@ -2,41 +2,55 @@ import React from 'react';
 import './userName.css';
 import MaleFemale from './MaleFemale';
 import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 
 
 
 class UserName extends React.Component {
   state = {
     testcomponent: false,
-    input:'',
-    error:''
+    input: '',
+    error: ''
   }
   render() {
-    const onChange = (e)=>{
+    const onChange = (e) => {
+      if (e.target.value.length > 15){
+        e.preventDefault();
+        return;
+      };
+      
       const letters = /^[A-Za-z]+$/;
-      if(!e.target.value.match(letters) && this.state.input.length){
+      if (!e.target.value.match(letters) && this.state.input.length > 1) {
 
-        this.setState({error:'Symbols are not allowed'})
+        this.setState({ error: 'Symbols are not allowed' })
         console.log(e.target.value)
-      }else{
+      } else {
 
-        this.setState({input:e.target.value,error:''})
+        this.setState({ input: e.target.value, error: '' })
       }
     }
-    const onchangeInput = ()=>{
-      if(!this.state.input){
-        this.setState({error:'Please write Your user Name'})
+
+    const onchangeInput = () => {
+      if (!this.state.input) {
+        this.setState({ error: 'Your name is required' })
       }
     }
+
     return (
       <div>
         {this.state.testcomponent ? <MaleFemale /> : <div className='un-main'>
           <div className='un-box'>
-            <h1>what is your <br /><span className='sp-1'>Name </span></h1>
+            <h1>What is your <br /><span className='sp-1'>Name </span></h1>
             <img src='https://cdn-icons-png.flaticon.com/512/3094/3094866.png' alt='logo' className='un-img' />
             <div className='inputsec'>
-              <span>{this.state.error}</span> <br/>
-              <input onChange={onChange} onBlur={onchangeInput} type='text' width={20} />
+              <span className='er'>{this.state.error}</span> <br />
+              <div className='input-main'>
+                <div className='un-icon'>
+                  <FaUser />
+                </div>
+                <input className='un-input' onInput={onChange} onBlur={onchangeInput} placeholder='Your name...' type='text' value={this.state.input} />
+                <div className='count'>{this.state.input.length}/15</div>
+              </div>
 
             </div>
             <Link to='/' className='btn'>create quiz</Link>
